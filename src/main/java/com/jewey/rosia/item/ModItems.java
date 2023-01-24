909cc9f105b0c736347f64c1ba04f793a5511e41
JeweyxD <joeymartinek@outlook.com>
2023-01-23 16:41:31 -0800

    replace overwritten items

--- src/main/java/com/jewey/rosia/item/ModItems.java
+++ src/main/java/com/jewey/rosia/item/ModItems.java
@@ -12,7 +12,35 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, Rosia.MOD_ID);
 
+
     public static final RegistryObject<Item> COPPER_WIRE = ITEMS.register("copper_wire",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> COPPER_COIL = ITEMS.register("copper_coil",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> NICHROME = ITEMS.register("nichrome_ingot",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> NICHROME_WIRE = ITEMS.register("nichrome_wire",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> HEAT_COIL = ITEMS.register("heat_coil",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> MAGNETITE_POWDER = ITEMS.register("magnetite_powder",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> COMPRESSED_MAGNETITE = ITEMS.register("compressed_magnetite",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> UNFIRED_FIRECLAY_INGOT = ITEMS.register("unfired_fireclay_ingot",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> INGOT_FIRECLAY_EMPTY = ITEMS.register("ingot_fireclay_empty",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> MAGNET = ITEMS.register("magnet",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
 
